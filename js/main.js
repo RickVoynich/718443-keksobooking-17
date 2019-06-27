9dd0c4958facf05ff32cf9a641e6c698bae69076
Rick <46853857+RickVoynich@users.noreply.github.com>
2019-06-27 04:09:23 +0300

    Создает массив

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 'use strict';
 
+
+// Функция генерации случайных чисел
+var getRandomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+
 // 1. Создайте массив, состоящий из 8 сгенерированных JS объектов, которые будут описывать похожие объявления неподалёку. Структура объектов должна быть следующей:
 
 // {
 //   "author": {
 //     "avatar": строка, адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
-//   },
-//   "offer": {
-//     "type": строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
 //   },
 
 //   "location": {
@@ -15,18 +19,58 @@
 //     "y": случайное число, координата y метки на карте от 130 до 630.
 //   }
 // }
+var PIN_QUANTITY = 8;
+var MAX_Y = 630;
+var MIN_Y = 130;
+var MIN_X = 0;
+var MAX_X = 500; //временное значение
+var PIN_WIDTH = 50; //временное значение
+var PIN_HEIGHT = 50; //временное значение
+
+var fillPin = function (avatar, type, x, y) {
+  return {
+    'author': {
+      'avatar': avatar
+    },
+    'offer': {
+      'type': type
+    },
+    'location': {
+      'x': x,
+      'y': y
+    }
+  }
+}
+
+var getAvatar = function (i) {
+  return 'img/avatars/user0' + (i + 1) + '.png';
+}
+var getType = function () {
+  var types = ['palace', 'flat', 'house', 'bungalo'];
+  return types[getRandomInt(0, types.length)];
+}
+
+var getX = function (minX, maxX) {
+  return getRandomInt(minX, maxX);
+}
+
+var getY = function (minY, maxY) {
+  return getRandomInt(minY, maxY);
+}
+
+var pins = [];
+for (var i = 0; i < PIN_QUANTITY; i++) {
+  pins.push(fillPin(getAvatar(i), getType(), getX(MIN_X, MAX_X - PIN_WIDTH / 2), getY(MIN_Y, MAX_Y - PIN_HEIGHT)));
+}
 
 
 
 
-// 2. У блока .map уберите класс .map--faded.
+
+// Перевод карты в активное состояние
 var mapBlock = document.querySelector('.map');
 mapBlock.classList.remove('map--faded');
 
-// Это временное решение, этот класс переключает карту из неактивного состояния в активное.
-// В последующих заданиях, в соответствии с ТЗ вы будете переключать режимы страницы: неактивный, в котором карта и форма заблокированы и активный режим,
-// в котором производится ввод данных и просмотр похожих объявлений. Сейчас для тестирования функции генерации похожих объявлений мы временно сымитируем активный режим,
-// а в последующих разделах запрограммируем его полностью.
 
 // 3. На основе данных, созданных в первом пункте, создайте DOM-элементы, соответствующие меткам на карте, и заполните их данными из массива.
 // Итоговую разметку метки .map__pin можно взять из шаблона #pin.
