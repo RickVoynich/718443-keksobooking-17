4f6ac84817c4df984901e22ace2df9d62a936864
Rick <46853857+RickVoynich@users.noreply.github.com>
2019-08-03 23:51:33 +0300

    Отправка данных на сервер

--- js/backend.js
+++ js/backend.js
@@ -2,6 +2,7 @@
 
 (function () {
   var GET_URL = 'https://js.dump.academy/keksobooking/data';
+  var POST_URL = 'https://js.dump.academy/keksobooking';
 
   var load = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
@@ -31,8 +32,25 @@
     xhr.send();
   };
 
+  var save = function (data, onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.open('POST', POST_URL);
+    xhr.send(data);
+  };
+
   window.backend = {
-    load: load
+    load: load,
+    save: save
   };
 
 })();
