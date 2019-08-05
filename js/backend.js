30a0f016126a2bbdf180dc13e3616f599637ccd1
Rick <46853857+RickVoynich@users.noreply.github.com>
2019-08-05 19:45:22 +0300

    Правки защита (Б1 - ТЗ 2.6 - частично)
    Если при отправке данных произошла ошибка запроса, показывается соответствующее сообщение.

--- js/backend.js
+++ js/backend.js
@@ -46,6 +46,18 @@
       }
     });
 
+    xhr.addEventListener('error', function () {
+      var main = document.querySelector('main');
+      var notice = document.querySelector('.notice');
+
+      var errorMessage = document.querySelector('#error')
+        .content
+        .querySelector('.error')
+        .cloneNode(true);
+
+      main.insertBefore(errorMessage, notice);
+    });
+
     xhr.open('POST', POST_URL);
     xhr.send(data);
   };
