95b3bac2c2a18cbb877eebd8e6d5d1e64efbab58
Rick <46853857+RickVoynich@users.noreply.github.com>
2019-08-07 04:30:32 +0300

    Правки защита (Д14)
    Отсутствует дублирование кода: повторяющиеся части кода переписаны как функции.

--- js/backend.js
+++ js/backend.js
@@ -4,10 +4,12 @@
 
   var GET_URL = 'https://js.dump.academy/keksobooking/data';
   var POST_URL = 'https://js.dump.academy/keksobooking';
+  var GET_METHOD = 'GET';
+  var POST_METHOD = 'POST';
   var STATUS = 200;
 
+  var createXhr = function (method, url,onSuccess, onError, data ) {
 
-  var load = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
@@ -21,26 +23,21 @@
 
     xhr.addEventListener('error', onError);
 
-    xhr.open('GET', GET_URL);
-    xhr.send();
+    xhr.open(method, url);
+
+    if (method === GET_METHOD) {
+      xhr.send()
+    } else {
+      xhr.send(data);
+    }
+  };
+
+  var load = function (onSuccess, onError) {
+    createXhr(GET_METHOD, GET_URL, onSuccess, onError);
   };
 
   var save = function (data, onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === STATUS) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', onError);
-
-    xhr.open('POST', POST_URL);
-    xhr.send(data);
+    createXhr(POST_METHOD, POST_URL,  onSuccess, onError, data);
   };
 
   window.backend = {
